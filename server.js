3f8b7ede83d8cc82793fe86cd32bac53865da648
Sujay <sujay26p@gmail.com>
2018-02-25 21:14:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,13 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article one',function(req,res){
+app.get('/article-one',function(req,res){
     res.send('Article one requested');
 });
-app.get('/article two',function(req,res){
+app.get('/article-two',function(req,res){
     res.send('Article two requested');
 });
-app.get('/article three',function(req,res){
+app.get('/article-three',function(req,res){
     res.send('Article three requested');
 });
 app.get('/ui/style.css', function (req, res) {
